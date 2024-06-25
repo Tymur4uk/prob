6f3b2c2b84775e1135c06de7a5a2192d409cdc57
Tymur4uk <109506961+Tymur4uk@users.noreply.github.com>
2024-06-25 14:36:41 +0300

    ==1==

---
+++ src/main/resources/web/js.js
@@ -0,0 +1,13 @@
+document.onmousemove = (e) => {
+	document.onclick = () => {
+		let circle = document.createElement('div');
+		circle.classList.add('click');
+		circle.style.left = e.pageX + 'px';
+		circle.style.top = e.pageY + 'px';
+		document.body.appendChild(circle);
+
+		setTimeout(() => {
+			circle.remove();
+		}, 1000);
+	};
+};
